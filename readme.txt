c9fe496314190ea1c922424bf7dde3f37cd7811a
tralce <dan@tralce.com>
2021-11-22 17:06:25 -0500

    Initial commit

---
+++ readme.txt
@@ -0,0 +1,38 @@
+Ever wanted to swap Costumes, Polymorphs, Mounts, Pets, or more on the fly? Now you can!
+
+Mounts
+[LIST]
+[*]Dwarven Spider
+[*]Ebon Dwarven Wolf
+[/LIST]
+
+Pets
+[LIST]
+[*]Dwarven Scarab
+[*]Dwarven Theodolite
+[/LIST]
+
+Polymorphs
+[LIST]
+[*]Pumpkin Spectre
+[*]Scarecrow Spectre
+[*]Werewolf Lord
+[*]Wraith-of-Crows
+[*]Iron Atronach
+[*]Factotum
+[*]Skeleton
+[*]Cadaverous Assassin
+[/LIST]
+
+Other
+[LIST]
+[*]Hide your Helm
+[/LIST]
+
+Grab my other addon, [URL="https://www.esoui.com/downloads/info2907-tralcesVanityKeybinds.html"]tralce's Collectible Keybinds[/URL], while you're at it!
+
+Feel free to recommend more in the comments! Ideally also with the Collectible ID, which you can get by linking your collectible in chat, and copying and pasting the link into your comment.
+
+Now includes some code that allows multi-key keybinds (AKA Chording).
+
+This is a fork of code65536's [URL="https://www.esoui.com/downloads/info2309-KeybindingMiscellaneousMementos.html"]Keybinding: Miscellaneous Mementos[/URL] addon.
---
+++ tralceVanity.lua
@@ -0,0 +1,43 @@
+tralceVanity = {
+  name = "tralceVanity",
+
+  collectibles = {
+    [1174] = "MOUNT_EBONWOLF",
+    [1185] = "MOUNT_DWEMERSPIDER",
+    [1232] = "PET_THEODOLYTE",
+    [8551] = "PET_DWEMER_SCARAB",
+    [  34] = "POLY_SKELETON",
+    [ 167] = "POLY_SCARECROW",
+    [ 168] = "POLY_PUMPKIN",
+    [ 176] = "POLY_WEREWOLF",
+    [ 387] = "POLY_CADAVASSASSIN",
+    [1388] = "POLY_WRAITHCROW",
+    [1480] = "POLY_FACTOTUM",
+    [9239] = "POLY_IRONATRO",
+    [5002] = "OTHER_HIDEHELM"
+
+  }
+}
+
+-- Force multi-key bindings. Shamelessly stolen from EssentialHousingTools.
+if not KEYBINDING_MANAGER.IsChordingAlwaysEnabled or not KEYBINDING_MANAGER:IsChordingAlwaysEnabled() then
+  function KEYBINDING_MANAGER:IsChordingAlwaysEnabled()
+    return true
+  end
+end
+
+local function OnAddOnLoaded( eventCode, addonName )
+  if (addonName ~= tralceVanity.name) then return end
+
+  EVENT_MANAGER:UnregisterForEvent(tralceVanity.name, EVENT_ADD_ON_LOADED)
+
+  for id, code in pairs(tralceVanity.collectibles) do
+    local name, _, _, _, unlocked = GetCollectibleInfo(id)
+    if (unlocked) then
+      ZO_CreateStringId("SI_BINDING_NAME_" .. code, zo_strformat(SI_COLLECTIBLE_NAME_FORMATTER, name))
+    end
+  end
+end
+
+EVENT_MANAGER:RegisterForEvent(tralceVanity.name, EVENT_ADD_ON_LOADED, OnAddOnLoaded)
+
